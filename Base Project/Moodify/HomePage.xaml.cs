70c6a9c54d0d46a4b6f70f7230eaabe671929bf6
thisIsMyGitHub <omegasayan@outlook.com>
2016-11-23 14:23:11 +1300

    version 2

---
+++ Base Project/Moodify/HomePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Moodify.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Moodify
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+        private async void ViewTimeline_Clicked(Object sender, EventArgs e)
+        {
+            List<Timeline> timelines = await AzureManager.AzureManagerInstance.GetTimelines();
+
+            TimelineList.ItemsSource = timelines;
+
+        }
+    }
+}
