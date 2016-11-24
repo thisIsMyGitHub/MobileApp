5b00fdfaab98e0341d8edf84451d3e592b58bb09
thisIsMyGitHub <omegasayan@outlook.com>
2016-11-24 20:05:56 +1300

    menu screen done

--- BP2/Base Project/Moodify/MenuPage.xaml.cs
+++ BP2/Base Project/Moodify/MenuPage.xaml.cs
@@ -14,5 +14,61 @@ namespace Moodify
         {
             InitializeComponent();
         }
+
+        // order buttons
+        private void OrderPizza(object sender, EventArgs e)
+        { 
+            int x = Int32.Parse(pizzaOrder.Text);
+            x++;
+            pizzaOrder.Text = x.ToString();
+
+        }
+        private void OrderPasta(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(pastaOrder.Text);
+            x++;
+            pastaOrder.Text = x.ToString();
+        }
+        private void OrderBurger(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(burgerOrder.Text);
+            x++;
+            burgerOrder.Text = x.ToString();
+        }
+        private void OrderCola(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(colaOrder.Text);
+            x++;
+            colaOrder.Text = x.ToString();
+        }
+
+
+        //down buttons
+        private void downPizza(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(pizzaOrder.Text);
+            if (x > 0) x--;
+            pizzaOrder.Text = x.ToString();
+
+        }
+        private void downPasta(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(pastaOrder.Text);
+            if (x>0)x--;
+            pastaOrder.Text = x.ToString();
+        }
+        private void downBurger(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(burgerOrder.Text);
+            if (x > 0) x--;
+            burgerOrder.Text = x.ToString();
+        }
+        private void downCola(object sender, EventArgs e)
+        {
+            int x = Int32.Parse(colaOrder.Text);
+            if (x > 0) x--;
+            colaOrder.Text = x.ToString();
+        }
+
     }
 }
